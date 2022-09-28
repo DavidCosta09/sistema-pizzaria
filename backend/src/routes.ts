d0c99ce7973c7f5c9f66c5efac8f70912d038c3b
David Costa <71845967+DavidCosta09@users.noreply.github.com>
2022-09-28 20:39:51 -0300

    finish order

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,6 +14,8 @@ import { AddItemController } from "./controllers/order/AddItemController";
 import { RemoveItemController } from "./controllers/order/RemoveItemController";
 import { SendOrderController } from "./controllers/order/SendOrderController";
 import { ListOrdersController } from "./controllers/order/ListOrdersController";
+import { DetailOrderController } from "./controllers/order/DetailOrderController";
+import { FinishOrderController } from "./controllers/order/FinishOrderController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -71,5 +73,17 @@ router.delete(
 router.put("/order/send", isAuthenticated, new SendOrderController().handle);
 
 router.get("/orders", isAuthenticated, new ListOrdersController().handle);
+
+router.get(
+  "/order/detail",
+  isAuthenticated,
+  new DetailOrderController().handle
+);
+
+router.put(
+  "/order/finish",
+  isAuthenticated,
+  new FinishOrderController().handle
+);
 
 export { router };
